59a52b4f2006b30562e90a6f7f1dfa85e1ee0d89
Owen Nelson <onelson@gmail.com>
2017-04-30 02:02:30 -0700

    adding users table for user account data

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,19 @@
+//! Kaizen Server is an HTTP service, mainly geared towards REST API interactions for a
+//! variety of data, including user auth, registration, repo management, ACL, I don't know what
+//! key management, etc.
 
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
+extern crate dotenv;
+extern crate pwhash;
 extern crate kaizen_core;
+
+mod auth;
+mod models;
+mod schema;
+
 
 fn main() {
     println!("Hello, world!");
